16bd7c1ba5472b2afcbb7789bd26e4635b8829ac
dordogh <dorothyordogh@dorothy-ordoghs-macbook-pro.local>
2014-06-15 19:20:10 -0700

    moved parser to server package

--- src/punishers/thirst/server/CSVReader.java
+++ src/punishers/thirst/server/CSVReader.java
@@ -1,4 +1,4 @@
-package punishers.thirst.data;
+package punishers.thirst.server;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/punishers/thirst/server/WaterFountain.java
+++ src/punishers/thirst/server/WaterFountain.java
@@ -1,4 +1,4 @@
-package punishers.thirst.data;
+package punishers.thirst.server;
 
 import java.util.HashSet;
 import java.util.Set;
