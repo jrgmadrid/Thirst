ac7a1af5564df18e5ebe742b6068246495f92bab
jacob <jrg.mdrd@gmail.com>
2014-06-18 11:52:06 -0700

    added the missing stuff

---
+++ src/punishers/thirst/client/CSVReaderService.java
@@ -0,0 +1,16 @@
+package punishers.thirst.client;
+
+import java.util.Set;
+
+import punishers.thirst.server.WaterFountain;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("csvreader")
+public interface CSVReaderService extends RemoteService{
+	public Set<WaterFountain> getWaterFountains();
+	public WaterFountain retrieveFromSet(int id);
+	public void updateData();
+	
+}
