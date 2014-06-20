447ce2fa4b19bbc74317ff3696a2847ac0c77de3
nani <nani@Lyralei>
2014-06-20 03:29:40 -0700

    Marker iteration of maps.js

--- src/punishers/thirst/client/maps.js
+++ src/punishers/thirst/client/maps.js
@@ -6,47 +6,196 @@
              |_|      
 
 The javascript map handler for Thirst
-*/
-/*
-getPoints - retrieve points from an element
 
-returns - an array of points
+////////////////////////////////////////
+
+   ___ _     _          _    
+  / __| |___| |__  __ _| |___
+ | (_ | / _ \ '_ \/ _` | (_-<
+  \___|_\___/_.__/\__,_|_/__/
+                             
 */
-function getPoints(){
-	var points = document.querySelector('#points').getAttribute('value').split(';');
-	return points;
+
+var fountains;
+var favorites;
+var map;
+var markers;
+google.maps.event.addDomListener(window, 'load', mapInitialize);
+
+/*
+
+  ___             _   _             
+ | __|  _ _ _  __| |_(_)___ _ _  ___
+ | _| || | ' \/ _|  _| / _ \ ' \(_-<
+ |_| \_,_|_||_\__|\__|_\___/_||_/__/
+                                    
+Function library
+TOC:
+1. Draw
+	- 1.1 drawMarkers
+2. Get
+	- 2.1 getFavorites
+	- 2.2 getFountains
+3. Find
+	- 3.1 findBounds
+	- 3.2 findCenter
+4. Map
+	- 4.1 mapInitialize
+	- 4.2 mapSetup
+*/                                    
+
+/*
+drawFavorites - draw favorite fountains
+*/
+function drawFavorites(){
+	mapClear();
+	markers = mapMarkers(favorites);
+	drawMarkers();
+}
+/*
+drawFountains - draw the whole set of fountains
+*/
+function drawFountains(){
+	mapClear();
+	markers = mapMarkers(fountains);
+	drawMarkers();
+}
+
+/*
+drawMarkers - draw the set of markers on the map
+*/
+function drawMarkers(){
+	for(var i=0; i<markers.length;i++){
+		markers[i].setMap(map);
+	}
+}
+
+/*
+getFavorites - retrieve favorites data
+
+Returns - an array of maps.LatLng objects
+*/
+function getFavorites(){
+	var fav = document.querySelector('#favorites').getAttribute('value').split(';');
+	favorites = [];
+	for(var i=0; i<fav.length;i++){
+		var x = parseFloat(fav[i].split(',')[0]);
+		var y = parseFloat(fav[i].split(',')[1]);
+
+		var coords = new google.maps.LatLng(x,y);
+		favorites.push(coords);
+	}
+	return favorites;
+}
+
+/*
+getFountains - retrieve fountain data
+
+Returns - an array of maps.LatLng objects
+*/
+function getFountains(){
+	var fnt = document.querySelector('#fountains').getAttribute('value').split(';');
+	fountains = [];
+	for(var i=0; i<fnt.length;i++){
+		var x = parseFloat(fnt[i].split(',')[0]);
+		var y = parseFloat(fnt[i].split(',')[1]);
+
+		var coords = new google.maps.LatLng(x,y);
+		fountains.push(coords);
+	}
+	return fountains;
+}
+
+
+/*
+findBounds - find the bounds to set the zoom level for tha map
+
+Params:
+	p: An array of points
+Returns:
+	A maps.LatLngBounds object
+*/
+function findBounds(p){
+
 }
 /*
 findCenter - find the center point of a set of points
 
 Params:
-	p: An array of points
+	p: An array of maps.LatLng points
 Returns:
 	A maps.LatLng object
 */
 function findCenter(p){
-	var highLat = 0;
-	var highLong = 0;
-	var lowLat = 180;
-	var lowLong = 180;
-
-	for(point in p){
-		var coords = point.split(',');
-		if(coords[0] > highLat)
-			highLat = coords[0];
-		if(coords[0] < lowLat)
-			lowLat = coords[0];
-		if(coords[1] > highLong)
-			highLong = coords[1];
-		if(coords[1] < lowLong)
-			lowLong = coords[1];
+	var highLat = p[0].lat();
+	var highLong =p[1].lng();
+	var lowLat = p[0].lat();
+	var lowLong = p[1].lng();
+	for(var i=0; i<p.length; i++){
+		var x = p[i].lng();
+		var y = p[i].lat();
+		if(y > highLat){
+			highLat = y;
+		}
+		if(y < lowLat){
+			lowLat = y;
+		}
+		if(x > highLong){
+			highLong = x;
+		}
+		if(x < lowLong){
+			lowLong = x;
+		}
 	}
-	var centerLat = (highLat+lowLat)/2;
-	var centerLong = (highLong + lowLong)/2;
+	var centerLat = (highLat + lowLat) / 2;
+	var centerLong = (highLong + lowLong) / 2;
 	var center = new google.maps.LatLng(centerLat, centerLong);
 	return center;
 }
 
+/*
+mapClear - clear markers from the map
+*/
+function mapClear(){
+	for(var i=0;i<markers.length;i++){
+		markers[i].setMap(null);
+	}
+}
+
+/*
+mapInitialize - initialize the map
+*/
+function mapInitialize(){
+	fountains = getFountains();
+	favorites = getFavorites();
+	var options = mapSetup(findCenter(fountains),10);
+	map = new google.maps.Map(document.querySelector('#map'),options);
+	markers = mapMarkers(fountains);
+	drawFountains();
+
+	/*var centerMark = new google.maps.Marker({
+		position: findCenter(points),
+		map: map,
+		title: "Center"});*/
+}
+
+/*
+mapMarkers - create an array of markers
+Params:
+	l - an array of LatLng objects
+Returns
+	An array of markers
+*/
+function mapMarkers(l){
+	var markers = [];
+	for(var i=0; i<l.length; i++){
+		var m = new google.maps.Marker({
+			position: l[i],
+		});
+		markers.push(m);
+	}
+	return markers;
+}
 /*
 mapSetup - setup mapOptions
 
@@ -54,20 +203,11 @@ Params:
 	c - center value to use
 	z - zoom level
 */
-function mapSetup(){
+function mapSetup(c, z){
 	var mapOptions = {
-		center: new google.maps.LatLng(-34.397, 150.644),
-		zoom: 10
+		center: c,
+		zoom: z
 	};
 	return mapOptions;
 }
-/*
-mapInitialize - initialize the map
-*/
-function mapInitialize(){
-	var options = mapSetup();
-	var map = new google.maps.Map(document.querySelector('#map'),options);
-	return map;
-}
 
-google.maps.event.addDomListener(window, 'load', mapInitialize);
