748149b61f4ea7280fad712cd476dad75b9c1b82
dordogh <dorothyordogh@dhcp-128-189-238-18.ubcsecure.wireless.ubc.ca>
2014-07-10 17:17:10 -0700

    Fixed file upload stuff
    Some things still need to be implemented, but the bare bones are there

--- src/punishers/thirst/server/CSVReaderServiceImpl.java
+++ src/punishers/thirst/server/CSVReaderServiceImpl.java
@@ -21,16 +21,19 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 @PersistenceCapable
 public class CSVReaderServiceImpl extends RemoteServiceServlet implements CSVReaderService {
 
-	/*
-	 * A set of water fountains that will be committed to the database once it is parsed
-	 */
-	@Persistent
-	private static Set<WaterFountain> waterFountains = new HashSet<WaterFountain>();
+	private static Set<WaterFountain> waterFountains;
 	
 	private static final PersistenceManagerFactory PMF = JDOHelper.getPersistenceManagerFactory("transactions-optional");
 	
 	/*
-	 * Get water fountains in the datastore
+	 * Get fountains in the class's field
+	 */
+	public static Set<WaterFountain> getFountains() {
+		return waterFountains;
+	}
+	
+	/*
+	 * Get water fountains from the datastore
 	 */
 	public Set<WaterFountain> getAllWaterFountains() {
 		PersistenceManager pm = getPersistenceManager();
@@ -56,11 +59,15 @@ public class CSVReaderServiceImpl extends RemoteServiceServlet implements CSVRea
 		return result;
 	}
 	
+	public CSVReaderServiceImpl() {
+		waterFountains = new HashSet<WaterFountain>();
+	}
+	
 	public void updateData() {
 		
 		try {
 			//The URL that has the data
-			URL dataUrl = new URL("http://puu.sh/9zQpH/222953ccfd.csv");
+			URL dataUrl = new URL("http://puu.sh/a5LdS.csv");
 			//Opens the URL where the data is held
 			URLConnection urlConnect = dataUrl.openConnection();
 			//Reads the data 
@@ -82,6 +89,71 @@ public class CSVReaderServiceImpl extends RemoteServiceServlet implements CSVRea
 			
 			Set<WaterFountain> fountains = new HashSet<WaterFountain>();
 			fountains = getAllWaterFountains();
+			
+			//Parses the rest of the file line by line
+			while ((line = csvFile.readLine()) != null) {
+				double lat;
+				double lon;
+				String location;
+				String maintainer;
+				
+				//Array of strings that is the info for each water fountain
+				String[] waterFountain = line.split(delim);
+				
+				//Takes each item in the array and sets it to each local variable
+				lat = Double.parseDouble(waterFountain[0]);
+				lon = Double.parseDouble(waterFountain[1]);
+				location = waterFountain[2];
+				maintainer = waterFountain[3];
+				
+				//Makes a new water fountain with all of the information
+				WaterFountain fountain = new WaterFountain(lat, lon, location, maintainer);
+				//Sets the ID of the water fountain which will be the key for the entity in the database
+				//fountain.setId();
+				//Adds the water fountain to the set which will then be stored in the database
+				//waterFountains.add(fountain);
+				
+				if (!fountains.contains(fountain)) {
+					PersistenceManager pm = getPersistenceManager();
+					try {
+						pm.makePersistent(fountain);
+					} finally {
+						pm.close();
+					}
+				}
+			}
+				
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void updateDataWithoutChecking() {
+		
+		try {
+			//The URL that has the data
+			URL dataUrl = new URL("http://puu.sh/a5LdS.csv");
+			//Opens the URL where the data is held
+			URLConnection urlConnect = dataUrl.openConnection();
+			//Reads the data 
+			InputStreamReader inStream = new InputStreamReader(urlConnect.getInputStream());
+			//adjusts the reading of the file
+			BufferedReader csvFile = new BufferedReader(inStream);
+			
+			//Set up the string in which each line will be read into
+			String line = "";
+			//Set up the variable at which point the string is split
+			String delim = ",";
+			
+			/*
+			 * The first line of the file is parsed into this variable
+			 * variable is a throw-away variable because first line doesn't
+			 * actually contain any useful information
+			 */
+			String firstLine = csvFile.readLine();
+			
 			//Parses the rest of the file line by line
 			while ((line = csvFile.readLine()) != null) {
 				double lat;
@@ -104,15 +176,6 @@ public class CSVReaderServiceImpl extends RemoteServiceServlet implements CSVRea
 				//fountain.setId();
 				//Adds the water fountain to the set which will then be stored in the database
 				waterFountains.add(fountain);
-				
-				if (!fountains.contains(fountain)) {
-					PersistenceManager pm = getPersistenceManager();
-					try {
-						pm.makePersistent(fountain);
-					} finally {
-						pm.close();
-					}
-				}
 			}
 				
 			
@@ -125,4 +188,5 @@ public class CSVReaderServiceImpl extends RemoteServiceServlet implements CSVRea
 	private PersistenceManager getPersistenceManager() {
 		  return PMF.getPersistenceManager();
 	}
+
 }
